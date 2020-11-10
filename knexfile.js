eca9e42b81f0be1b0c81553f99f8c55c1feeb585
Noah Franco <noah@macbooks-mbp.lan>
2020-11-10 15:58:14 -0500

    pointed knexfile to production

--- knexfile.js
+++ knexfile.js
@@ -19,7 +19,8 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DB_URL,
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
     migration: {
       directory: "./migrations"
     },
